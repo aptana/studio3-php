752d61b9d7d879c2fbb06c49b06f844a1ed0a57e
Shalom Gibly <sgibly@aptana.com>
2012-01-04 16:50:53 -0800

    APSTUD-3553 - enables 'Open Declaration' internally in a PHP file that was opened directly from an FTP connection

--- plugins/com.aptana.editor.php/src/com/aptana/editor/php/internal/contentAssist/mapping/CodeLocation.java
+++ plugins/com.aptana.editor.php/src/com/aptana/editor/php/internal/contentAssist/mapping/CodeLocation.java
@@ -1,5 +1,7 @@
 package com.aptana.editor.php.internal.contentAssist.mapping;
 
+import org.eclipse.core.filesystem.IFileStore;
+
 import com.aptana.editor.php.internal.contentAssist.PHPTokenType;
 import com.aptana.parsing.lexer.Lexeme;
 
@@ -8,11 +10,18 @@ public class CodeLocation implements ICodeLocation
 
 	private String path;
 	private Lexeme<PHPTokenType> lexeme;
+	private IFileStore remoteFileStore;
 
 	public CodeLocation(String path, Lexeme<PHPTokenType> lexeme)
 	{
 		this.path = path;
 		this.lexeme = lexeme;
+	}
+
+	public CodeLocation(IFileStore remoteFileStore, Lexeme<PHPTokenType> lexeme)
+	{
+		this.lexeme = lexeme;
+		this.remoteFileStore = remoteFileStore;
 	}
 
 	public String getFullPath()
@@ -25,4 +34,12 @@ public class CodeLocation implements ICodeLocation
 		return lexeme;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see com.aptana.editor.php.internal.contentAssist.mapping.ICodeLocation#getRemoteFileStore()
+	 */
+	public IFileStore getRemoteFileStore()
+	{
+		return remoteFileStore;
+	}
 }
