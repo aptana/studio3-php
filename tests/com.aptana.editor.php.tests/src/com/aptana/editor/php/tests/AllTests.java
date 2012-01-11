98b9814c92386cb7ec138fa0cc23eb0a905e582b
Shalom Gibly <sgibly@aptana.com>
2012-01-11 15:15:59 -0800

    Re-add the PHP scopes test. Split the PHPDoc-tags test into 3 tests to avoid opening 30 editors.

--- tests/com.aptana.editor.php.tests/src/com/aptana/editor/php/tests/AllTests.java
+++ tests/com.aptana.editor.php.tests/src/com/aptana/editor/php/tests/AllTests.java
@@ -12,7 +12,7 @@ public class AllTests
 	{
 		TestSuite suite = new TestSuite(AllTests.class.getName());
 		// $JUnit-BEGIN$
-		// suite.addTestSuite(PHPScopesTest.class);
+		suite.addTestSuite(PHPScopesTest.class);
 		suite.addTestSuite(PHTMLSourcePartitionScannerTest.class);
 		suite.addTestSuite(PHTMLSourcePartitionScannerModificationTest.class);
 		suite.addTestSuite(PHTMLTagScannerTest.class);
