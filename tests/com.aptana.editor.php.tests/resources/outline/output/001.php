52448acb706331443679a9e5fe7cee9c725be98c
Shalom Gibly <sgibly@aptana.com>
2012-03-15 13:57:58 -0700

    Remove the outline test files (dealing with new-lines)

--- tests/com.aptana.editor.php.tests/resources/outline/output/001.php
+++
@@ -1,4 +0,0 @@
-PHPFunctionParseNode [com.aptana.contenttype.php, (6, 23)]
-PHPFunctionParseNode [com.aptana.contenttype.php, (44, 70)]
-PHPClassParseNode [com.aptana.contenttype.php, (26, 72)]
-ParseRootNode [com.aptana.contenttype.php, (0, 75)]
