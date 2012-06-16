ad9c4669902ecfa32d33548a15fe9e91ed9a5799
Ingo Muschenetz <ingo@muschenetz.com>
2012-06-16 08:40:01 -0700

    Reverting fixes for APSTUD-4778 to use a new LRU cache. This is an attempt to see if it helps address APSTUD-4861.

--- plugins/com.aptana.editor.php/src/com/aptana/editor/php/internal/parser/PHPParseState.java
+++ plugins/com.aptana.editor.php/src/com/aptana/editor/php/internal/parser/PHPParseState.java
@@ -4,9 +4,7 @@ import org2.eclipse.php.internal.core.PHPVersion;
 
 import com.aptana.editor.php.core.model.ISourceModule;
 import com.aptana.editor.php.internal.core.builder.IModule;
-import com.aptana.parsing.IParseStateCacheKey;
 import com.aptana.parsing.ParseState;
-import com.aptana.parsing.ParseStateCacheKey;
 
 /**
  * A PHP parse state implementation with the ability to set and get a PHP version.
@@ -19,7 +17,6 @@ public class PHPParseState extends ParseState implements IPHPParseState
 	private PHPVersion phpVersion;
 	private IModule module;
 	private ISourceModule sourceModule;
-	private static IParseStateCacheKey cacheKey = new PHPParseStateCacheKey();
 
 	/*
 	 * (non-Javadoc)
@@ -58,31 +55,5 @@ public class PHPParseState extends ParseState implements IPHPParseState
 	public ISourceModule getSourceModule()
 	{
 		return this.sourceModule;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see com.aptana.parsing.ParseState#getCacheKey(java.lang.String)
-	 */
-	@Override
-	public IParseStateCacheKey getCacheKey(String contentTypeId)
-	{
-		return cacheKey;
-	}
-
-	/**
-	 * PHP ParseStateCacheKey that always return true for requiresReparse()
-	 */
-	public static class PHPParseStateCacheKey extends ParseStateCacheKey
-	{
-		/*
-		 * (non-Javadoc)
-		 * @see com.aptana.parsing.ParseStateCacheKey#requiresReparse(com.aptana.parsing.IParseStateCacheKey)
-		 */
-		@Override
-		public boolean requiresReparse(IParseStateCacheKey newCacheKey)
-		{
-			return true;
-		}
 	}
 }
