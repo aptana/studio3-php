a4dae4095ae6e728831429084fc9e438d25fba26
Shalom Gibly <sgibly@appcelerator.com>
2012-08-28 17:24:39 -0700

    APSTUD-4892 - Enable content-assist for a function with the same name that exist in different files. Enable multiple hyperlinks selection when Ctrl+Hover a function call.

--- plugins/com.aptana.editor.php/src/com/aptana/editor/php/internal/indexer/BuildPathElementEntriesFilter.java
+++ plugins/com.aptana.editor.php/src/com/aptana/editor/php/internal/indexer/BuildPathElementEntriesFilter.java
@@ -1,6 +1,6 @@
 /**
  * Aptana Studio
- * Copyright (c) 2005-2011 by Appcelerator, Inc. All Rights Reserved.
+ * Copyright (c) 2005-2012 by Appcelerator, Inc. All Rights Reserved.
  * Licensed under the terms of the GNU Public License (GPL) v3 (with exceptions).
  * Please see the license.html included with this distribution for details.
  * Any modifications to this file must keep this entire header intact.
@@ -107,7 +107,7 @@ public class BuildPathElementEntriesFilter implements IElementEntriesFilter
 				IModule module = e.getModule();
 				boolean added = false;
 				String entryPrefix = ElementsIndexingUtils.getFirstNameInPath(e.getEntryPath());
-				if (!entryToModule.containsKey(entryPrefix) || entryToModule.get(entryPrefix) == module)
+				if (!entryToModule.containsKey(entryPrefix) || entryToModule.get(entryPrefix) != module)
 				{
 					if (activeBuildPaths.size() != 0)
 					{
