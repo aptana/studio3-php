53b450ec2cc1fc4c2d4cb3085bca4b4df06f62e6
Shalom Gibly <sgibly@appcelerator.com>
2013-05-28 14:05:49 -0700

    Fix NPE

--- plugins/com.aptana.editor.php/src/com/aptana/editor/php/internal/contentAssist/PHPContentAssistProcessor.java
+++ plugins/com.aptana.editor.php/src/com/aptana/editor/php/internal/contentAssist/PHPContentAssistProcessor.java
@@ -1829,9 +1829,9 @@ public class PHPContentAssistProcessor extends CommonContentAssistProcessor impl
 
 					// Check for traits vs. classes/interfaces here. In case we are in a trait 'use', or trait 'use'
 					// block, we would like to see only trait types.
-					if (proposalContext != null && PHPContextCalculator.TRAIT_USE_PROPOSAL_CONTEXT_TYPE.equals(proposalContext.getType())
-							|| PHPContextCalculator.TRAIT_USE_BLOCK_PROPOSAL_CONTEXT_TYPE.equals(proposalContext
-									.getType()))
+					if (proposalContext != null
+							&& (PHPContextCalculator.TRAIT_USE_PROPOSAL_CONTEXT_TYPE.equals(proposalContext.getType()) || PHPContextCalculator.TRAIT_USE_BLOCK_PROPOSAL_CONTEXT_TYPE
+									.equals(proposalContext.getType())))
 					{
 						IContextFilter contextFilter = proposalContext.getContextFilter();
 						for (IElementEntry elementEntry : classTypeEntries)
