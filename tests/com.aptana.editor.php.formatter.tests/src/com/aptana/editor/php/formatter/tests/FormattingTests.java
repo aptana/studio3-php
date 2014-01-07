fa6632fb2235546f4c5ba74f685f8264a4afc8fb
Christopher Williams <cwilliams@appcelerator.com>
2014-01-07 09:32:32 -0500

    Fix up tests

--- tests/com.aptana.editor.php.formatter.tests/src/com/aptana/editor/php/formatter/tests/FormattingTests.java
+++ tests/com.aptana.editor.php.formatter.tests/src/com/aptana/editor/php/formatter/tests/FormattingTests.java
@@ -1,22 +1,23 @@
 package com.aptana.editor.php.formatter.tests;
 
+import java.util.Arrays;
+
+import org.junit.runners.Parameterized.Parameters;
+
 import com.aptana.editor.common.formatting.AbstractFormatterTestCase;
 
 public class FormattingTests extends AbstractFormatterTestCase
 {
-	// Turn this flag on for development only (used to generate the formatted files)
-	// To generate formatted files, place php files under the 'formatting' folder and run these tests from the
-	// com.aptana.editor.php.formatter.tests plugin
-	// NOTE: Ensure that the contents section ends with a newline, or the generation may not work.
-	private static boolean INITIALIZE_MODE = false;
-	// Turning on the overwrite will re-generate the formatted block and overwrite it into the test files.
-	// This is a drastic move that will require a review of the output right after to make sure we have the
-	// right formatting for all the test file, so turn it on at your own risk.
-	private static boolean OVERWRITE_MODE = false;
 
 	private static String FORMATTER_FACTORY_ID = "com.aptana.editor.php.formatterFactory"; //$NON-NLS-1$
 	private static String TEST_BUNDLE_ID = "com.aptana.editor.php.formatter.tests"; //$NON-NLS-1$
 	private static String FILE_TYPE = "php"; //$NON-NLS-1$
+
+	@Parameters(name = "{0}")
+	public static Iterable<Object[]> data()
+	{
+		return Arrays.asList(AbstractFormatterTestCase.getFiles(TEST_BUNDLE_ID, FILE_TYPE));
+	}
 
 	/*
 	 * (non-Javadoc)
@@ -46,25 +47,5 @@ public class FormattingTests extends AbstractFormatterTestCase
 	protected String getFileType()
 	{
 		return FILE_TYPE;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see com.aptana.editor.common.formatting.AbstractFormatterTestCase#isOverriteMode()
-	 */
-	@Override
-	protected boolean isOverriteMode()
-	{
-		return OVERWRITE_MODE;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see com.aptana.editor.common.formatting.AbstractFormatterTestCase#isInitializeMode()
-	 */
-	@Override
-	protected boolean isInitializeMode()
-	{
-		return INITIALIZE_MODE;
 	}
 }
