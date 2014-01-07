4216992a7668fce614723f9730817fd875b9ee57
Christopher Williams <cwilliams@appcelerator.com>
2014-01-07 09:32:25 -0500

    Fix up tests

--- tests/com.aptana.editor.php.epl.tests/src/org/eclipse/php/core/tests/AllCoreTests.java
+++ tests/com.aptana.editor.php.epl.tests/src/org/eclipse/php/core/tests/AllCoreTests.java
@@ -8,8 +8,6 @@ import org.eclipse.php.core.tests.codeassist.PHPContentAssistProcessorTests;
 import org.eclipse.php.core.tests.dom_ast.parser.DomParserTests;
 import org.eclipse.php.core.tests.markoccurrence.MarkOccurrenceTests;
 
-import com.aptana.editor.php.formatter.tests.FormattingTests;
-
 public class AllCoreTests
 {
 	public static Test suite()
@@ -18,7 +16,6 @@ public class AllCoreTests
 		// $JUnit-BEGIN$
 		suite.addTest(DomParserTests.suite());
 		suite.addTest(MarkOccurrenceTests.suite());
-		suite.addTest(new FormattingTests().suite());
 		suite.addTest(CodeAssistTests.suite());
 		suite.addTestSuite(PHPContentAssistProcessorTests.class);
 		// $JUnit-END$
