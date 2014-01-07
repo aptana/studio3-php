ae0e2ada9e88e6a3c8edd8b3e51e243c993b7c7a
Christopher Williams <cwilliams@appcelerator.com>
2014-01-07 09:32:25 -0500

    Fix up tests

--- tests/com.aptana.editor.php.formatter.tests/src/com/aptana/editor/php/formatter/tests/AllTests.java
+++ tests/com.aptana.editor.php.formatter.tests/src/com/aptana/editor/php/formatter/tests/AllTests.java
@@ -7,18 +7,12 @@
  */
 package com.aptana.editor.php.formatter.tests;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
 
+@RunWith(Suite.class)
+@SuiteClasses({ FormattingTests.class })
 public class AllTests
 {
-
-	public static Test suite()
-	{
-		TestSuite suite = new TestSuite("Test for com.aptana.editor.php.formatter.tests"); //$NON-NLS-1$
-		// $JUnit-BEGIN$
-		suite.addTest(new FormattingTests().suite());
-		// $JUnit-END$
-		return suite;
-	}
 }
